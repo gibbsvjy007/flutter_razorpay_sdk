e4c3665045d83e16766d86114127885371fa6bbc
unknown <atchula.nataraj@live.in>
2018-10-10 21:28:27 +0530

    solution for onsuccesspayment probably not implemented

--- example/android/app/src/main/java/com/sevenre/flutterrazorpaysdkexample/MainActivity.java
+++ example/android/app/src/main/java/com/sevenre/flutterrazorpaysdkexample/MainActivity.java
@@ -3,11 +3,21 @@ package com.sevenre.flutterrazorpaysdkexample;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.razorpay.PaymentResultListener;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity implements PaymentResultListener {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+    
+  public void onPaymentSuccess(String razorpayPaymentID) {
+    Log.i(TAG, "Payment Successful: " + razorpayPaymentID);
+  }
+
+  public void onPaymentError(int code, String response) {
+    Log.i(TAG, "Payment Error: " + response);
+    Log.i(TAG, "Payment Error: " + code);
   }
 }
